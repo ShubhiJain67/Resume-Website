8539e98d107074968c77a408c5972affc3365051
Shubhi Jain <shubhi.jain@altudo.co>
2020-12-13 04:31:45 +0530

    Minor Changes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 activeLink="home-link";
 
 function getPage(pageLink){
-    document.getElementById(activeLink).classList.remove("active");
-    document.getElementById(pageLink).classList.add("active");
     let pageHide=activeLink.substring(0,activeLink.lastIndexOf("-"))+"-page";
     let pageShow=pageLink.substring(0,pageLink.lastIndexOf("-"))+"-page";
     console.log(activeLink+" "+pageHide);
     console.log(pageLink+" "+pageShow);
+    document.getElementById(activeLink).classList.remove("active");
+    document.getElementById(pageLink).classList.add("active");
     document.getElementById(pageHide).style.display="none";
     document.getElementById(pageShow).style.display="block";
     activeLink=pageLink;
